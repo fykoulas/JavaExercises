1da8b6d69b9d02bc5bb6afa38b5901d98ac986bf
fykoulas <spyrosfykas@gmail.com>
2024-10-29 20:08:52 +0200

    Java Exercises

--- src/gr/aueb/cf/ch1/ScannerApp.java
+++ src/gr/aueb/cf/ch1/ScannerApp.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class ScannerApp {
     public static void main(String[] args) {
+        //Αντιστοιχίζουμε το πληκτρολόγιο (System.in) στη μεταβλητή in
         Scanner in = new Scanner(System.in);
 
+        //Εντολές
         System.out.print("Enter your name: ");
         String name = in.nextLine();
 
         System.out.print("Enter your age: ");
         int age = in.nextInt();
 
-        System.out.print("Enter your height in cm: ");
+        System.out.print("Enter your height in m: ");
         double height = in.nextDouble();
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("Height: " + height + " cm");
+        System.out.println("Height: " + height + " m");
 
         in.close();
     }
